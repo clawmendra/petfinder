10b9c363bd1283cb455fd483cafa8f5e620f0c58
Almendra Clawson <mendrav@gmail.com>
2024-12-04 22:20:32 -0700

    finished app

--- my_plots.py
+++ my_plots.py
@@ -1,15 +1,48 @@
-import requests
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
+import plotly.express as px
 
 adoptions = pd.read_csv('animals_data.csv')
 
-# Filter by dogs and cats tabs
 cats = adoptions[adoptions['species'] == 'Cat']
 dogs = adoptions[adoptions['species'] == 'Dog']
 
-st.selectbox = ("Filter by age range:",
-                ("Baby", "Young", "Adult", "Senior"),
-                )
+
+def animal_plot(df, sex, ages):
+    data = df.copy()
+
+    if not ages:
+        ages = ['Baby', 'Young', 'Adult', 'Senior']
+    
+    new_data = data[
+        (data['gender'].str.contains(sex, case=False)) & 
+        (data['age'].isin(ages))
+    ]
+
+    age_counts = new_data.groupby('age').size().reset_index()
+    age_counts.columns = ['Age Range', 'Count']
+
+    age_order = ["Baby", "Young", "Adult", "Senior"]
+    age_counts['Age Range'] = pd.Categorical(age_counts['Age Range'], 
+                                           categories=age_order, 
+                                           ordered=True)
+    pets = age_counts.sort_values('Age Range')
+
+    fig = px.bar(
+        pets,
+        x='Age Range', 
+        y='Count',
+        title=f'Distribution of {sex} pets by Age',
+        color='Age Range',
+        color_discrete_sequence=px.colors.qualitative.Set3
+    )
+
+    fig.update_layout(
+        xaxis_title="Age Category",
+        yaxis_title="Count",
+        showlegend=False,
+        plot_bgcolor='white',
+        bargap=0.2
+    )
+
+    return fig
--- st-pet.py
+++ st-pet.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -7,7 +8,7 @@ from my_plots import *
 
 adoptions = pd.read_csv('animals_data.csv')
 
-st.title('Pet Available for Adoption in UT')
+st.title('Pets Available for Adoption in UT')
 # Filter by dogs and cats tabs
 cats = adoptions[adoptions['species'] == 'Cat']
 dogs = adoptions[adoptions['species'] == 'Dog']
@@ -15,7 +16,46 @@ dogs = adoptions[adoptions['species'] == 'Dog']
 tab1, tab2 = st.tabs(['Cats', 'Dogs'])
 
 with tab1:
-    age = st.selectbox("Filter by age range:",
-                ("Baby", "Young", "Adult", "Senior"),
-                )
-    age_data = cats[cats['age']==age].copy()
+    age_selected = st.multiselect(
+        "Select Age Range for Cats",
+        ["Baby", "Young", "Adult", "Senior"]
+    )
+    genders = ['Male', 'Female']
+    gender_option = st.radio(
+        "Gender for Cat",
+        genders
+    )
+    st.header(f'Distribution of {gender_option} Cats by Age')
+    cat_fig = animal_plot(df=cats, sex=gender_option, ages=age_selected)
+    st.plotly_chart(cat_fig)
+
+with tab2:
+    age_selected2 = st.multiselect(
+        "Select Age Range for Dogs",
+        ["Baby", "Young", "Adult", "Senior"]
+    )
+    genders2=['Male', "Female"]
+    gender_option2 = st.radio(
+        "Gender for Dog",
+        genders2
+    )
+    
+    st.header(f'Distribution of {gender_option} Dogs by Age')
+    dog_fig = animal_plot(df=dogs, sex=gender_option2, ages=age_selected2)
+    st.plotly_chart(dog_fig)
+
+    # Sidebar Wideget
+    st.sidebar.header('Look at a random pet!')
+    if st.sidebar.button('Click Here', type = 'primary'):
+        random_num = np.random.randint(0, len(adoptions))
+        random_pet = adoptions.iloc[random_num]
+        st.sidebar.write(f'Organization: {random_pet.iloc[-1]}')
+        st.sidebar.write(f'Name: {random_pet.iloc[4]}')
+        st.sidebar.write(f'Species: {random_pet.iloc[3]}')
+        st.sidebar.write(f'Gender: {random_pet.iloc[-4]}')
+        st.sidebar.write(f'Age: {random_pet.iloc[5]}')
+
+    else:
+        st.sidebar.write('')
+  
+
